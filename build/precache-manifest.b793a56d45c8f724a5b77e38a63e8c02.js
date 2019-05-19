997cf62eb4e425c753bc84ae55591b51089ad9ca
nireo <eemil.lehtikangas1@gmail.com>
2019-05-19 22:36:52 +0300

    added frontend

---
+++ build/precache-manifest.b793a56d45c8f724a5b77e38a63e8c02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ef8e25076f0ea5d536581bcebfe637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6708a5f4775103cc1846",
+    "url": "/static/css/main.05f5cfe6.chunk.css"
+  },
+  {
+    "revision": "717095a3ea21a6bbdee1",
+    "url": "/static/js/2.3d2452bb.chunk.js"
+  },
+  {
+    "revision": "6708a5f4775103cc1846",
+    "url": "/static/js/main.ed3e2623.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
