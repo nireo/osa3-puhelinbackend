04291830ab33137761735e74d03311a5455b248a
nireo <eemil.lehtikangas1@gmail.com>
2019-05-22 22:27:57 +0300

    added find to show all database entries

--- mongo.js
+++ mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
-console.log(process.argv)
-
+// check for password parameter
 if ( process.argv.length < 3 ) {
     console.log("give password as argument")
     process.exit(1)
@@ -12,6 +11,9 @@ const password = process.argv[2]
 const url = `mongodb+srv://fullstack:${password}@cluster0-ww9qd.mongodb.net/test?retryWrites=true`
 
 mongoose.connect(url, {useNewUrlParser: true})
+
+// check if no added parameters except password
+
 
 const personSchema = new mongoose.Schema({
     name: String,
@@ -27,7 +29,16 @@ const person = new Person({
     id: 7
 })
 
-person.save().then(response => {
-    console.log(`lisätään ${process.argv[3]} numero ${process.argv[4]} luetteloon`)
-    mongoose.connection.close()
-})
+if (process.argv.length < 4 && process.argv.length === 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    person.save().then(response => {
+        console.log(`lisätään ${process.argv[3]} numero ${process.argv[4]} luetteloon`)
+        mongoose.connection.close()
+    })
+}
